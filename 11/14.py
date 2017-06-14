76e82a7d704cf63803511e7b93e9ef7007ef5498
fflffl2017 <fflffl2015@163.com>
2017-06-14 20:48:27 +0800

    new

---
+++ 11/14.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+def f(n):
+    flist = [0, 1, 1]
+    if 1 == n or 2 == n:
+        return 1
+    else:
+        for i in range(2, n):
+            flist.append(f(i) + flist[i - 1])
+    return flist[n]
+
+
+print f(6)  
+

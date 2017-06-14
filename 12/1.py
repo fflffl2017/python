7225401a49e99ec144aafb264f6d6b12a1e75cd2
fflffl2017 <fflffl2015@163.com>
2017-06-14 22:21:12 +0800

    12

---
+++ 12/1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+f=__import__('sys',fromlist=['platform'])
+platform=f.platform
+print f.modules
+print platform
+
+
---
+++ 12/2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def importAS(n):
+    return __import__(n)
+
+if __name__=='__main__':
+    n='os'
+    n=importAS(n)
+    print n.path
